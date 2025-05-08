2236b1c3748cc502019077fc4fdaef87582b4f27
asmabenbali <assmabenbali5@gmail.com>
2025-05-08 17:59:40 +0100

    Test : modification ajoutée

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+console.log("Test Git");
+
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
